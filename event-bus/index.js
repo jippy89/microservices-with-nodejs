9b62129d57a6be0393a853f0fcaa0c5aca5d04e4
Jip Irfandy <jip.irfandy@dayadimensi.co.id>
2021-07-20 23:06:39 +0700

    Step 1: Update `event-bus` service
    Changing the uRL for "posts" service statically
    Then:
    1. Rebuild image
    2. Push image to Docker Hub
    3. Run `kubectl rollout restart deployment event-bus-srv

--- event-bus/index.js
+++ event-bus/index.js
@@ -17,10 +17,10 @@ app.post('/events', (req, res) => {
 
   events.push(event)
 
-  axios.post('http://localhost:4000/events', event).catch(err => console.log(err))
-  axios.post('http://localhost:4001/events', event).catch(err => console.log(err))
-  axios.post('http://localhost:4002/events', event).catch(err => console.log(err))
-  axios.post('http://localhost:4003/events', event).catch(err => console.log(err))
+  axios.post('http://posts-clusterip-srv:4000/events', event).catch(err => console.log(err))
+  // axios.post('http://localhost:4001/events', event).catch(err => console.log(err))
+  // axios.post('http://localhost:4002/events', event).catch(err => console.log(err))
+  // axios.post('http://localhost:4003/events', event).catch(err => console.log(err))
 
   res.status(201).send({
     status: 'OK'
